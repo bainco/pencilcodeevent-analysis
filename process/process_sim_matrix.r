2419b80a7ecaa4933ccf462bf0db8cd3a994fd57
Connor Bain <bainco@email.sc.edu>
2016-05-23 13:46:54 -0500

    Modularized and cleaned.

--- process/process_sim_matrix.r
+++ process/process_sim_matrix.r
@@ -1,4 +1,4 @@
-processSimTable <- function(CONDITION, clean=TRUE) {
+processSimTable <- function(CONDITION, CLUSTERMETHOD="complete", clean=TRUE) {
 
 setwd("/Users/connorbain/nudropbox/pencilcc_analysis/process/graph_output")
 
@@ -19,7 +19,20 @@ rownames(mydata) <- colnames(mydata)
 
 if (clean && (identical("master", CONDITION) || identical("hybrid", CONDITION))) {
     # Remove 413 Outlier in Hybrid and Master
+    mydata <- mydata[-(match("X418", rownames(mydata))), -(match("X418", colnames(mydata)))]
     mydata <- mydata[-(match("X413", rownames(mydata))), -(match("X413", colnames(mydata)))]
+    mydata <- mydata[-(match("X11", rownames(mydata))), -(match("X11", colnames(mydata)))]
+    mydata <- mydata[-(match("X13", rownames(mydata))), -(match("X13", colnames(mydata)))]
+
+}
+if (clean && (identical("block", CONDITION) || identical("master", CONDITION))) {
+	
+	mydata <- mydata[-(match("X706", rownames(mydata))), -(match("X706", colnames(mydata)))]
+}
+
+if (clean && (identical("text", CONDITION) || identical("master", CONDITION))) {
+	
+	mydata <- mydata[-(match("X828", rownames(mydata))), -(match("X828", colnames(mydata)))]
 }
 
 ## Compute Euclidean Distance CMD
@@ -46,12 +59,33 @@ heatmap(matrix, symm=TRUE)
 dev.off()
 
 distCor <- function(x) as.dist(x)
+myhClust <- function(x) hclust(x, method=CLUSTERMETHOD)
 pdf(paste(CONDITION, "_heatmap_symm_nodist.pdf", sep=""))
-heatmap(matrix, symm=TRUE, distfun=distCor)
+heatmap(matrix, symm=TRUE, distfun=distCor, hclustfun=myhClust)
 dev.off()
 
 pdf(paste(CONDITION, "_heatmap.pdf", sep=""))
 heatmap(matrix)
 dev.off()
 setwd("/Users/connorbain/nudropbox/pencilcc_analysis/process/")
+
+x <- hclust(distCor(matrix), method=CLUSTERMETHOD)
+#plot(x)
+
+pdf(paste(CONDITION, "_clustering_assessment.pdf", sep=""))
+asw <- numeric(20)
+## Note that "k=1" won't work!
+for (k in 2:20)
+  asw[k] <- pam(distCor(matrix), k) $ silinfo $ avg.width
+k.best <- which.max(asw)
+cat("silhouette-optimal number of clusters:", k.best, "\n")
+
+plot(1:20, asw, type= "h", main = "pam() clustering assessment",
+     xlab= "k  (# clusters)", ylab = "average silhouette width")
+axis(1, k.best, paste("best",k.best,sep="\n"), col = "red", col.axis = "red")
+dev.off()
+
+clusplot(pam(distCor(matrix), k.best))
+
+return(pam(distCor(matrix), k.best))
 }
